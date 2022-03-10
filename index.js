1f7b66adc5831efca04d8f46898f86d71dc09e5d
Nordwall <tobiasnordwall@gmail.com>
2022-03-10 15:53:39 +0100

    I switched from using a for loop to map to create the anserArray

--- index.js
+++ index.js
@@ -4,13 +4,11 @@ const input1 = document.querySelector(".input-1");
 const input2 = document.querySelector(".input-2");
 
 function ost(secretWord, guess) {
-    let array1 = secretWord.split("");
-    let array2 = guess.split("");
-    let answerArray = [];
-    for (let k = 0; k < array2.length; k++) {
-        answerArray.push(array2[k] + " incorrect")
-    }
-    console.log(answerArray)
+    let array1 = secretWord.toUpperCase().split("");
+    let array2 = guess.toUpperCase().split("");
+
+    let answerArray = array2.map(item => item + " incorrect");
+
     for (let i = 0; i < array2.length; i++) {
         if (array2[i] === array1[i]) {
             answerArray.splice([i], 1, array2[i] + " " + "correct")
@@ -18,7 +16,6 @@ function ost(secretWord, guess) {
             array2[i] = "&";
         }
     }
-    console.log(answerArray)
     for (let i = 0; i < array2.length; i++) {
         if (array1.includes(array2[i])) {
             for (let j = 0; j < array2.length; j++) {
@@ -26,10 +23,9 @@ function ost(secretWord, guess) {
                     array1[j] = "?";
             }
             answerArray.splice([i], 1, array2[i] + " " + " missplaced")
-            console.log(answerArray)
         }
     }
-
+    console.log(answerArray)
 }
 
-ost("hello", "hlelo");
+ost("cykla", "hallå");
