6716686c205314d41041511a08bfceb0a83ecb3d
Nordwall <tobiasnordwall@gmail.com>
2022-03-10 17:23:34 +0100

    I moved the function into a own module and made some minor changes

--- index.js
+++ index.js
@@ -1,31 +1,5 @@
+import checkWord from "./src/checkWord.js";
 
-const inputButton = document.querySelector(".outputBtn");
-const input1 = document.querySelector(".input-1");
-const input2 = document.querySelector(".input-2");
+let svar = checkWord("cykla", "kalle");
 
-function ost(secretWord, guess) {
-    let array1 = secretWord.toUpperCase().split("");
-    let array2 = guess.toUpperCase().split("");
-
-    let answerArray = array2.map(item => item + " incorrect");
-
-    for (let i = 0; i < array2.length; i++) {
-        if (array2[i] === array1[i]) {
-            answerArray.splice([i], 1, array2[i] + " " + "correct")
-            array1[i] = "!";
-            array2[i] = "&";
-        }
-    }
-    for (let i = 0; i < array2.length; i++) {
-        if (array1.includes(array2[i])) {
-            for (let j = 0; j < array2.length; j++) {
-                if (array1[j] === array2[i])
-                    array1[j] = "?";
-            }
-            answerArray.splice([i], 1, array2[i] + " " + " missplaced")
-        }
-    }
-    console.log(answerArray)
-}
-
-ost("cykla", "hallå");
+console.log(svar)
---
+++ src/checkWord.js
@@ -0,0 +1,37 @@
+
+export default function (secretWord, guess) {
+    let array1 = secretWord.toUpperCase().split("");
+    let array2 = guess.toUpperCase().split("");
+
+    let answerArray = array2.map(item => (
+        {
+            letter: item,
+            result: " incorrect"
+        }
+    ));
+
+    for (let i = 0; i < array2.length; i++) {
+        if (array2[i] === array1[i]) {
+            answerArray.splice([i], 1, {
+                letter: array2[i],
+                result: " correct"
+            })
+            array1[i] = "!";
+            array2[i] = "&";
+        }
+    }
+    for (let i = 0; i < array2.length; i++) {
+        if (array1.includes(array2[i])) {
+            for (let j = 0; j < array2.length; j++) {
+                if (array1[j] === array2[i])
+                    array1[j] = "?";
+            }
+            answerArray.splice([i], 1, {
+                letter: array2[i],
+                result: " missplaced"
+            })
+        }
+    }
+    return answerArray;
+}
+
