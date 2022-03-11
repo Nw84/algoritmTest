3cee8c670849059626ffa752f7226a5648901b90
Nordwall <tobiasnordwall@gmail.com>
2022-03-11 15:36:35 +0100

    i have documented my testing strategy inside the test

--- tests/checkWordTest.spec.js
+++ tests/checkWordTest.spec.js
@@ -1,5 +1,21 @@
 
 import checkWord from "../src/checkWord.js";
+/* Testing strategy 
+
+*First i wanted to check if the function gives the correct response if the correct word is guessed
+
+*Secondly i wanted to check if the function gives the correct response if the word is reversed, 
+so that i know that the letters are correctly labeled missplaced if they are. 
+
+*In my third test i wanted to check that the function handles repeated letters correctly, so that they are correctly labeled as correct, missplaced and incorrect. So i used three L´s in the hidden word and four in the guess to test that they got the correct label.
+
+*In my fourth test i wanted to check that the function handles lowercased and uppercased inputs correctly and that it does not affect the result. 
+
+*In my fifth test i wanted to check that the function returns the correct error message if the guess is longer or shorter than the secret word. 
+
+*The last test is just a copy of Richards test example from the assignment, to test if i get the same result 
+
+*/
 
 test("1: function gives the correct response if the correct word is guessed", () => {
     let response = checkWord("cykla", "cykla");
