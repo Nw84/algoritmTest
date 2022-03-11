41dd8d1bb92aca318ef987320587f7a3a79e6abd
Nordwall <tobiasnordwall@gmail.com>
2022-03-11 10:50:18 +0100

    I have added my five tests

--- tests/wordCheck.spec.js
+++ tests/wordCheck.spec.js
@@ -1,7 +1,7 @@
 
 import checkWord from "../src/checkWord.js";
 
-test("function gives the correct response if the correct word is guessed", () => {
+test("1: function gives the correct response if the correct word is guessed", () => {
     let response = checkWord("cykla", "cykla");
 
     let expectedResponse = [
@@ -24,3 +24,121 @@ test("function gives the correct response if the correct word is guessed", () =>
 
     expect(response).toEqual(expectedResponse);
 })
+
+test("2: function gives the correct response if its the right letters, but in the wrong order", () => {
+    let response = checkWord("hammare", "erammah");
+
+    let expectedResponse = [
+        {
+            letter: "E", result: "missplaced"
+        },
+        {
+            letter: "R", result: "missplaced"
+        },
+        {
+            letter: "A", result: "missplaced"
+        },
+        {
+            letter: "M", result: "correct"
+        },
+        {
+            letter: "M", result: "missplaced"
+        },
+        {
+            letter: "A", result: "missplaced"
+        },
+        {
+            letter: "H", result: "missplaced"
+        },
+    ]
+
+    expect(response).toEqual(expectedResponse);
+})
+
+test("3: function gives the correct response if several of the same letters are guessed", () => {
+    let response = checkWord("llploooos", "oolllolop");
+
+    let expectedResponse = [
+        {
+            letter: "O", result: "missplaced"
+        },
+        {
+            letter: "O", result: "missplaced"
+        },
+        {
+            letter: "L", result: "missplaced"
+        },
+        {
+            letter: "L", result: "correct"
+        },
+        {
+            letter: "L", result: "missplaced"
+        },
+        {
+            letter: "O", result: "correct"
+        },
+        {
+            letter: "L", result: "incorrect"
+        },
+        {
+            letter: "O", result: "correct"
+        },
+        {
+            letter: "P", result: "missplaced"
+        },
+
+    ]
+
+    expect(response).toEqual(expectedResponse);
+})
+
+test("4: function gives the correct response if the secret word is lowercased and the guess is uppercased", () => {
+    let response = checkWord("akuten", "ALENnA");
+
+    let expectedResponse = [
+        {
+            letter: "A", result: "correct"
+        },
+        {
+            letter: "L", result: "incorrect"
+        },
+        {
+            letter: "E", result: "missplaced"
+        },
+        {
+            letter: "N", result: "missplaced"
+        },
+        {
+            letter: "N", result: "incorrect"
+        },
+        {
+            letter: "A", result: "incorrect"
+        },
+    ]
+
+    expect(response).toEqual(expectedResponse);
+})
+
+test("5: Richards test example from the assignment", () => {
+    let response = checkWord("cykla", "hallå");
+
+    let expectedResponse = [
+        {
+            letter: "H", result: "incorrect"
+        },
+        {
+            letter: "A", result: "missplaced"
+        },
+        {
+            letter: "L", result: "incorrect"
+        },
+        {
+            letter: "L", result: "correct"
+        },
+        {
+            letter: "Å", result: "incorrect"
+        },
+    ]
+
+    expect(response).toEqual(expectedResponse);
+}) 
